d3b0dee5633ac99be89f4c5d42cec918c6f1319d
Christian Paesante <christian.paesante@tesisquare.com>
2019-05-23 15:18:17 +0200

    Fixed pactBroker.config

--- test/contract-testing/pactBroker.config.js
+++ test/contract-testing/pactBroker.config.js
@@ -1,5 +1,13 @@
+let isAvailable;
+if (process.env.PACT_BROKER_AVAILABLE === 'true')
+    isAvailable = true;
+else
+    isAvailable = false;
+
+
 const broker = {
     url: process.env.PACT_BROKER_URL || 'http://192.168.99.100',
+    isAvailable,
 };
 
 module.exports = broker;
