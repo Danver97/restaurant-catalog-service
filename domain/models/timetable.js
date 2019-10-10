8c621b750026922d50845096f199c7cb2e154f58
Christian Paesante <christian.paesante@tesisquare.com>
2019-10-11 00:10:01 +0200

    Timetable models implemented and tested

---
+++ domain/models/timetable.js
@@ -0,0 +1,184 @@
+class Timetable {
+    constructor() {
+        this.days = {};
+    }
+
+    setDays(days) {
+        if (!days)
+            throw new Error(`Missing the following params: days`);
+        const newDays = {};
+        for (let i = 1; i <= 7; i++) {
+            if (!days[i])
+                throw new Error(`Day ${i} not found`);
+            if (!(days[i] instanceof DayTimetable))
+                throw new Error(`Day ${i} is not instance of DayTimetable`);
+            if (days[i].dayOfWeek.toInt() !== i)
+                throw new Error(`Day ${i} has corresponding index of ${days[i].dayOfWeek.toInt()}`);
+            newDays[i] = days[i];
+        }
+        this.days = newDays;
+    }
+
+    setDay(day) {
+        if (!(day instanceof DayTimetable))
+            throw new Error(`day param must be instace of DayTimetable`);
+        this.days[day.dayOfWeek.toInt()] = day;
+    }
+
+    getDay(dayIndex) {
+        if (typeof dayIndex !== 'number')
+            throw new Error(`dayIndex must be a number`);
+        return this.days[dayIndex];
+    }
+}
+
+class DayOfWeek {
+    constructor(dayIndex) {
+        if (typeof dayIndex !== 'number')
+            throw new Error('dayIndex param must be a number');
+        let day = Math.abs(dayIndex) || 1;
+        this.dayIndex = day > 7 ? Math.floor(day / 7) : day;
+    }
+    toInt() {
+        return this.dayIndex;
+    }
+    static get MONDAY() {
+        return new DayOfWeek(1);
+    }
+    static get TUESDAY() {
+        return new DayOfWeek(2);
+    }
+    static get WEDNESDAY() {
+        return new DayOfWeek(3);
+    }
+    static get THURSDAY() {
+        return new DayOfWeek(4);
+    }
+    static get FRIDAY() {
+        return new DayOfWeek(5);
+    }
+    static get SATURDAY() {
+        return new DayOfWeek(6);
+    }
+    static get SUNDAY() {
+        return new DayOfWeek(7);
+    }
+}
+
+class DayTimetable {
+    constructor(dayOfWeek, openings) {
+        if (!dayOfWeek)
+            throw new Error(`Missing the following paramters:${dayOfWeek ? '' : ' dayOfWeek'}`);
+        if (!(dayOfWeek instanceof DayOfWeek))
+            throw new Error('dayOfWeek must be an instance of DayOfWeek')
+        if (openings) {
+            if (!Array.isArray(openings) || (openings.length > 0 && !(openings[0] instanceof OpeningInteval)))
+                throw new Error('opening must be an array of Opening instances');
+            this.openings = openings
+        } else
+            this.openings = [];
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    setOpenings(openings) {
+        if (!openings)
+            throw new Error('Missing the following paramters: openings');
+        if (!Array.isArray(openings) || (openings.length > 0 && !(openings[0] instanceof OpeningInteval)))
+            throw new Error('opening must be an array of Opening instances');
+        this.openings = openings
+    }
+
+    addOpening(opening) {
+        if (!opening)
+            throw new Error(`Missing the following paramters: opening`);
+        if (!(opening instanceof OpeningInteval))
+            throw new Error(`opening param must be instace of Opening`);
+        this.openings.push(opening);
+    }
+}
+
+class OpeningInteval {
+    constructor(opening, closing) {
+        if (!opening || !closing)
+            throw new Error(`Missing the following constructor parameters:${opening ? '' : ' opening'}${closing ? '' : ' closing'}`);
+        if (!(opening instanceof OpeningHour))
+            throw new Error(`opening param must be instance of OpeningHour`);
+        if (!(closing instanceof OpeningHour))
+            throw new Error(`closing param must be instance of OpeningHour`);
+        if (!opening.before(closing))
+            throw new Error(`opening time must be before closing`);
+        this.opening = opening;
+        this.closing = closing;
+    }
+}
+
+class OpeningHour {
+    constructor(h, m) {
+        if ((!h && h !== 0) || (!m && m !== 0))
+            throw new Error(`Missing the following constructor parameters:${h ? '' : ' h'}${m ? '' : ' m'}`);
+        this.h = h;
+        this.m = m;
+    }
+
+    setHours(newH) {
+        if (!newH)
+            throw new Error('Missing the following paramters: newH');
+        if (typeof newH !== 'number')
+            throw new Error('newH param must be a number');
+        this.h = newH % 24;
+    }
+
+    setMinutes(newM) {
+        if (!newM)
+            throw new Error('Missing the following paramters: newM');
+        if (typeof newM !== 'number')
+            throw new Error('newM param must be a number');
+        const newH = Math.floor(newM / 60);
+        this.setHours(this.h + newH);
+        this.m = newM % 60;
+    }
+
+    before(obj) {
+        if (!(obj instanceof OpeningHour))
+            throw new Error('obj is not a OpeningHour instance');
+        if (this.h !== obj.h)
+            return this.h < obj.h;
+        return this.m < obj.m;
+    }
+
+    equals(obj) {
+        if (!(obj instanceof OpeningHour))
+            throw new Error('obj is not a OpeningHour instance');
+        return this.h === obj.h && this.m === obj.m;
+    }
+    
+    after(obj) {
+        if (!(obj instanceof OpeningHour))
+            throw new Error('obj is not a OpeningHour instance');
+        if (this.h !== obj.h)
+            return this.h > obj.h;
+        return this.m > obj.m;
+    }
+
+    clone() {
+        return new OpeningHour(this.h, this.m);
+    }
+
+    compareTo(obj) {
+        if (!(obj instanceof OpeningHour))
+            throw new Error('obj is not a OpeningHour instance and it\'s not comparable');
+        if (this.h !== obj.h)
+            return this.h < obj.h ? -1 : 1;
+        if (this.m === obj.m)
+            return 0;
+        return this.m < obj.m ? -1 : 1;
+    }
+}
+
+module.exports = {
+    Timetable,
+    DayTimetable,
+    OpeningInteval,
+    OpeningHour,
+    DayOfWeek,
+};
---
+++ test/timetable.test.js
@@ -0,0 +1,226 @@
+const assert = require('assert');
+const timetableModule = require('../domain/models/timetable');
+
+const Timetable = timetableModule.Timetable;
+const DayTimetable = timetableModule.DayTimetable;
+const OpeningInteval = timetableModule.OpeningInteval;
+const OpeningHour = timetableModule.OpeningHour;
+const DayOfWeek = timetableModule.DayOfWeek;
+
+describe('Timetable module unit test', function () {
+
+    context('OpeningHour class test', () => {
+        it('check if constructor works', () => {
+            assert.throws(() => new OpeningHour(), Error);
+            const h = 15;
+            const m = 0;
+            const oh = new OpeningHour(h,m);
+            assert.strictEqual(oh.h, h);
+            assert.strictEqual(oh.m, m);
+        });
+
+        it('check setHour works', () => {
+            const h = 15;
+            const m = 0;
+            const oh = new OpeningHour(h,m);
+            assert.throws(() => oh.setHours(), Error);
+            assert.throws(() => oh.setHours(''), Error);
+            oh.setHours(16);
+            assert.strictEqual(oh.h, 16);
+            oh.setHours(24);
+            assert.strictEqual(oh.h, 0);
+            oh.setHours(30);
+            assert.strictEqual(oh.h, 6);
+        });
+
+        it('check setHour works', () => {
+            const h = 15;
+            const m = 0;
+            const oh = new OpeningHour(h,m);
+            assert.throws(() => oh.setMinutes(), Error);
+            assert.throws(() => oh.setMinutes(''), Error);
+            oh.setMinutes(16);
+            assert.strictEqual(oh.m, 16);
+            oh.setMinutes(75);
+            assert.strictEqual(oh.m, 15);
+            assert.strictEqual(oh.h, h+1);
+        });
+
+        it('check before works', () => {
+            const oh1 = new OpeningHour(15,0);
+            const oh2 = new OpeningHour(16,0);
+            const oh3 = new OpeningHour(15,15);
+            assert.throws(() => oh1.before(), Error);
+            assert.throws(() => oh1.before({}), Error);
+            assert.ok(oh1.before(oh2));
+            assert.ok(oh1.before(oh3));
+            assert.ok(oh3.before(oh2));
+            assert.ok(!oh3.before(oh1));
+            assert.ok(!oh2.before(oh1));
+        });
+        it('check equals works', () => {
+            const oh1 = new OpeningHour(15,0);
+            const oh2 = new OpeningHour(15,0);
+            const oh3 = new OpeningHour(15,15);
+            assert.throws(() => oh1.equals(), Error);
+            assert.throws(() => oh1.equals({}), Error);
+            assert.ok(oh1.equals(oh2));
+            assert.ok(!oh1.equals(oh3));
+            assert.ok(!oh3.equals(oh1));
+        });
+
+        it('check after works', () => {
+            const oh1 = new OpeningHour(15,0);
+            const oh2 = new OpeningHour(16,0);
+            const oh3 = new OpeningHour(15,15);
+            const oh4 = new OpeningHour(15,15);
+            assert.throws(() => oh1.after(), Error);
+            assert.throws(() => oh1.after({}), Error);
+            assert.ok(oh2.after(oh1));
+            assert.ok(oh2.after(oh3));
+            assert.ok(!oh3.after(oh2));
+            assert.ok(oh3.after(oh1));
+            assert.ok(!oh3.after(oh4));
+        });
+
+        it('check clone works', () => {
+            const oh1 = new OpeningHour(15,0);
+            assert.deepStrictEqual(oh1.clone(), oh1);
+        });
+
+        it('check compareTo works', () => {
+            const oh1 = new OpeningHour(15,0);
+            const oh2 = new OpeningHour(16,0);
+            const oh3 = new OpeningHour(15,15);
+            const oh4 = new OpeningHour(15,15);
+            assert.throws(() => oh1.compareTo(), Error);
+            assert.throws(() => oh1.compareTo({}), Error);
+            assert.strictEqual(oh1.compareTo(oh2), -1);
+            assert.strictEqual(oh1.compareTo(oh3), -1);
+            assert.strictEqual(oh2.compareTo(oh1), 1);
+            assert.strictEqual(oh3.compareTo(oh4), 0);
+        });
+    });
+
+    context('OpeningInterval class test', () => {
+        it('check if constructor works', () => {
+            const opening = new OpeningHour(15,0);
+            const closing0 = new OpeningHour(15,0);
+            const closing1 = new OpeningHour(14,0);
+            const closing = new OpeningHour(20,15);
+            assert.throws(() => new OpeningInteval(), Error);
+            assert.throws(() => new OpeningInteval(opening), Error);
+            assert.throws(() => new OpeningInteval(opening, closing0), Error);
+            assert.throws(() => new OpeningInteval(opening, closing1), Error);
+            const oInterval = new OpeningInteval(opening, closing);
+            assert.strictEqual(oInterval.opening.h, 15);
+            assert.strictEqual(oInterval.opening.m, 0);
+            assert.strictEqual(oInterval.closing.h, 20);
+            assert.strictEqual(oInterval.closing.m, 15);
+        });
+    });
+
+    context('DayTimetable class test', () => {
+        const opening1 = new OpeningHour(11,30);
+        const closing1 = new OpeningHour(13,15);
+        const opening2 = new OpeningHour(15,0);
+        const closing2 = new OpeningHour(20,15);
+        const interval1 = new OpeningInteval(opening1, closing1);
+        const interval2 = new OpeningInteval(opening2, closing2);
+
+        it('check constructor works', () => {
+            assert.throws(() => new DayTimetable(), Error);
+            assert.throws(() => new DayTimetable(DayOfWeek.MONDAY, {}), Error);
+            assert.throws(() => new DayTimetable(DayOfWeek.MONDAY, [1]), Error);
+            let dayTimetable = new DayTimetable(DayOfWeek.MONDAY);
+            assert.deepStrictEqual(dayTimetable.dayOfWeek, DayOfWeek.MONDAY);
+            assert.deepStrictEqual(dayTimetable.openings, []);
+            dayTimetable = new DayTimetable(DayOfWeek.MONDAY, [interval1]);
+            assert.deepStrictEqual(dayTimetable.openings, [interval1]);
+        });
+
+        it('check addOpening works', () => {
+            let dayTimetable = new DayTimetable(DayOfWeek.MONDAY);
+            assert.throws(() => dayTimetable.addOpening(), Error);
+            assert.throws(() => dayTimetable.addOpening({}), Error);
+            dayTimetable.addOpening(interval1);
+            assert.deepStrictEqual(dayTimetable.openings, [interval1]);
+            dayTimetable.addOpening(interval2);
+            assert.deepStrictEqual(dayTimetable.openings, [interval1, interval2]);
+            
+            dayTimetable = new DayTimetable(DayOfWeek.MONDAY, [interval1]);
+            dayTimetable.addOpening(interval2);
+            assert.deepStrictEqual(dayTimetable.openings, [interval1, interval2]);
+        });
+
+        it('check setOpenings works', () => {
+            let dayTimetable = new DayTimetable(DayOfWeek.MONDAY);
+            assert.throws(() => dayTimetable.setOpenings(), Error);
+            assert.throws(() => dayTimetable.setOpenings({}), Error);
+            assert.throws(() => dayTimetable.setOpenings([1,2]), Error);
+
+            dayTimetable.setOpenings([interval1]);
+            assert.deepStrictEqual(dayTimetable.openings, [interval1]);
+            dayTimetable.setOpenings([interval1, interval2]);
+            assert.deepStrictEqual(dayTimetable.openings, [interval1, interval2]);
+            
+            dayTimetable = new DayTimetable(DayOfWeek.MONDAY, [interval1]);
+            dayTimetable.setOpenings([interval1, interval2]);
+            assert.deepStrictEqual(dayTimetable.openings, [interval1, interval2]);
+        });
+    });
+
+    context('Timetable class test', () => {
+        const opening1 = new OpeningHour(11,30);
+        const closing1 = new OpeningHour(13,15);
+        const opening2 = new OpeningHour(15,0);
+        const closing2 = new OpeningHour(20,15);
+        const interval1 = new OpeningInteval(opening1, closing1);
+        const interval2 = new OpeningInteval(opening2, closing2);
+        const monday = new DayTimetable(DayOfWeek.MONDAY, [interval1, interval2]);
+        const tuesday = new DayTimetable(DayOfWeek.TUESDAY, [interval1, interval2]);
+        const wednesday = new DayTimetable(DayOfWeek.WEDNESDAY, [interval1, interval2]);
+        const thursday = new DayTimetable(DayOfWeek.THURSDAY, [interval1, interval2]);
+        const friday = new DayTimetable(DayOfWeek.FRIDAY, [interval1, interval2]);
+        const saturday = new DayTimetable(DayOfWeek.SATURDAY, [interval1, interval2]);
+        const sunday = new DayTimetable(DayOfWeek.SUNDAY, [interval1, interval2]);
+
+        it('check if constructor works', () => {
+            const timetable = new Timetable();
+            assert.deepStrictEqual(timetable.days, {});
+        });
+
+        it('check if setDay works', () => {
+            const timetable = new Timetable();
+            assert.throws(() => timetable.setDay(), Error);
+            assert.throws(() => timetable.setDay({}), Error);
+            timetable.setDay(monday);
+            assert.deepStrictEqual(timetable.days, { 1: monday });
+        });
+
+        it('check if getDay works', () => {
+            const timetable = new Timetable();
+            timetable.days = { 1: monday };
+            assert.deepStrictEqual(timetable.getDay(1), monday);
+        });
+
+        it('check if setDays works', () => {
+            const timetable = new Timetable();
+            assert.throws(() => timetable.setDays(), Error);
+            assert.throws(() => timetable.setDays({}), Error);
+            assert.throws(() => timetable.setDays({ 1: {} }), Error);
+            assert.throws(() => timetable.setDays({ 1: tuesday }), Error);
+            const newDays = {
+                1: monday,
+                2: tuesday,
+                3: wednesday,
+                4: thursday,
+                5: friday,
+                6: saturday,
+                7: sunday,
+            };
+            timetable.setDays(newDays);
+            assert.deepStrictEqual(timetable.days, newDays);
+        });
+    });
+});
